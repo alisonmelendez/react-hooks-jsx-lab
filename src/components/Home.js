df134ec10affde19d82100e6c471c338b3f77c81
Alison Melendez <alisonmelendez132@gmail.com>
2022-02-01 10:19:18 -0500

    commit tree 01333d0e6b8d757b641e50463254edbd090d56d5

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const h1Color = {
+  color: 'firebrick'
+};
+
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div>
+    <div id="home">
+      <h1 style={h1Color}> {name} is a Web Developer from {city}</h1>
+    </div>
+  </div>
+  );
 }
 
 export default Home;
